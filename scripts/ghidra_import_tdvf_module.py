cc1df1910d03da7bb43eb618ecd4fce6fe8a79dd
hemx0147 <rnx781@gmail.com>
2022-12-20 11:33:27 +0100

    [ghidra import] finalize main logic of script

--- scripts/ghidra_import_tdvf_module.py
+++ scripts/ghidra_import_tdvf_module.py
@@ -4,28 +4,23 @@ from tdvf_module import *
 import argparse
 
 
-# GHIDRA_BIN = os.environ['GHIDRA_ROOT'] + '/support/analyzeHeadless'
-# PROJ_DIR = os.environ['KAFL_WORKDIR'] + '/traces/ghidra'
-# PROJ_NAME = 'cov_analysis'
-# target = os.environ['TDVF_ROOT'] + '/Build/IntelTdx/DEBUG_GCC5/X64/SecMain.debug'
-# baseaddr = '0xfffcc000'
-# jfile = os.environ['BKC_ROOT'] + '/tdvf-modules.json'
-
-# def ghidra_import_targets(targets:dict, proj_dir:str, proj_name:str):
-#     '''load a set of target binaries at an offset into the ghidra project'''
-#     for target, address in targets.items():
-#         # command line to execute
-#         args = [GHIDRA_BIN, proj_dir, proj_name, '-import', target, '-overwrite', '-loader', 'ElfLoader', '-loader-imagebase', baseaddr]
-#         completed_proc = subprocess.run(args, capture_output=True, encoding='utf-8')
-#         print(completed_proc.stdout)
-#         print(completed_proc.stderr)
+def ghidra_import_targets(proj_dir:str, proj_name:str, targets:dict) -> Tuple[str, str]:
+    '''load a set of target binaries at an offset into the ghidra project and return process stdout & stderr'''
+    ghidra_bin = os.environ['GHIDRA_ROOT'] + '/support/analyzeHeadless'
+    for target, address in targets.items():
+        # command line to execute
+        print(f'ghidra import {target} @ {address}')
+        args = [ghidra_bin, proj_dir, proj_name, '-import', target, '-overwrite', '-loader', 'ElfLoader', '-loader-imagebase', address]
+        # TODO: fix ghidra invocation (currently only first target is imported but nothing more)
+        completed_proc = subprocess.run(args, capture_output=True, encoding='utf-8')
+        return completed_proc.stdout, completed_proc.stderr
 
 def get_targets_from_file(file_path:str):
     if file_path.endswith('.json'):
         # file contains list of TdvfModule objects in json format
         mt = TdvfModuleTable()
         mt.read_from_file(file_path)
-        targets = dict((module.d_path, module.img_base) for module in mt.modules.values())
+        targets = dict((module.d_path, str(module.img_base)) for module in mt.modules.values())
     else:
         # file line format: "<binary-path> <base-address>"
         with open(file_path, 'r') as f:
@@ -80,5 +75,7 @@ if __name__ == "__main__":
     else:
         targets = get_targets_from_file(file_path)
 
-    for binary, address in targets.items():
-        print(f'ghidra load {binary} @ {address}')
+    proj_dir = workdir + '/traces/ghidra'
+    proj_name = 'cov_analysis'
+    print(targets)
+    ghidra_import_targets(proj_dir, proj_name, targets)
